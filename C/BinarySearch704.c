f992ced0669bb3b7f0cb7ebd722d4b092c18ce22
nirja007354 <sarunjutha_bcs29@mepcoeng.ac.in>
2026-09-05 23:29:27 +0530

    Create BinarySearch704.c

---
+++ C/BinarySearch704.c
@@ -0,0 +1,23 @@
+int search(int* nums, int numsSize, int target) {
+    int mid=0;
+    int left=0;
+    int right=numsSize-1;
+    while(left<=right)
+    {
+        mid=(left+right)/2;
+        if(nums[mid]==target)
+        {
+            return mid;
+        }
+        else if(nums[mid]<target)
+        {
+            left=mid+1;
+        }
+        else
+        {
+            right=mid-1;
+        }
+    }
+    return -1;
+    
+}
